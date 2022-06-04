448dc3b78d3bbe1f4fe77e5d0d96916699b371c0
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-06-04 15:46:46 +0200

    s

--- code/main.py
+++ code/main.py
@@ -1,2 +1,20 @@
+from re import X
+
+
 print("das ist ein kalender")
 x = int(input("geben sie die anzahl der tage ein für die sie planen möchten"))
+print(X)
+print("ist diese anzahl richtig?")  
+print("ja oder nein")
+y = input()
+if y == "ja":
+    print("okay")
+    print("wollen sie den kalender als pdf oder als txt anzeigen?") #pdf oder txt
+    z = int(input())    #pdf oder txt
+    if z == 1:
+        print("pdf")
+        
+    elif z == 2:
+        print("txt")
+    else:
+        print("fehler")
